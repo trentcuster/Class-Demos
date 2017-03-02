90e6568480a4fb11ce46e03e4f3198e29d012217
trentcuster <tbcuster@gmail.com>
2017-03-02 10:23:49 -0700

    Objects day 1

---
+++ JavaScript/Objects/ObjectProject.js
@@ -0,0 +1,10 @@
+var game = {
+    difficultyE:"easy",
+    difficultyM:"Medium",
+    difficultyH:"Hard",
+    difficultyEx:"Extreme",
+    shooter:"First Person",
+    shooter2:"Third Person",
+    sensitivity:"0-100",
+    layout:"Controls"
+};
