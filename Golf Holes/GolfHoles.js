e34b0cff957eddf5e49d7f6ad05e5978c7b4508c
trentcuster <tbcuster@gmail.com>
2017-03-31 10:24:39 -0600

    golf holes

--- Golf Holes/GolfHoles.js
+++ Golf Holes/GolfHoles.js
@@ -3,10 +3,11 @@ var numHoles = 18;
 
 $(function(){
     for(var c =1; c <= numHoles; c++){
-        $(".rightBox").append("<div class = 'col"+ c + "'> hole " + c +"</div>");
+        $(".rightBox").append("<div class = 'col"+ c + "'> hole " + c +"</di>");
     }
 
     for(var p = 1; p <= numPlayers; p++){
+        $(".leftbox").append("<div class='playername'>player" + p + "</div>");
         for(var h = 1; h <= numHoles; h++){
             $(".col" + h).append("<input class='holebox' type='text' id='player" + p + "hole" + h + "'/>");
         }
