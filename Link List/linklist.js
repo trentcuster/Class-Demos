6c98b554c9d763f1b9b4a6cb3dfe080a725aff52
trentcuster <tbcuster@gmail.com>
2017-04-04 10:15:21 -0600

    link list

--- Link List/linklist.js
+++ Link List/linklist.js
@@ -1,33 +1,56 @@
-function LinkedList(){
+function LinkedList() {
     var head = null;
     this.length = 0;
     this.insert = insert;
     this.remove = remove;
     this.find = find;
 
-    function insert(data){
+
+
+    function insert(data) {
+        var myNode = new Node(data);
+        if(head != null) {
+            var node = head,
+                prev = null;
+            while (node && node.data.name.toLowerCase() < myNode.data.name.toLowerCase()) {
+
+                    prev = node;
+                    node = node.next;
+            }
+            if (!prev) {
+                myNode.next = node;
+                head = myNode;
+                this.length++;
+            }
+            else {
+                prev.next = myNode;
+                myNode.next = node;
+                this.length++;
+            }
+        }
+        else {
+            head = myNode;
+            this.length++;
+            return;
+        }
+    }
+
+    function remove(id) {
 
     }
-    function remove(id){
 
-    }
-    function find(name){
+    function find(name) {
         var node = head;
-        while(node != null && node.date.name != name){
+        while (node != null && node.date.name != name) {
             node = node.next;
         }
 
-        var returnObject = node? node.data : null;
+        return node ? node.data : null;
     }
 
-    function Node(data){
+    function Node(data) {
         this.data = data;
         this.next = null;
     }
-    function Node(data){
-        this.data = data;
-        this.next = nulls;
-    }
-
-
 }
+
