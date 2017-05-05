bbacb61a7405526f0c651f2532c315bc70a15868
trentcuster <tbcuster@gmail.com>
2017-05-05 10:22:15 -0600

    day 2 started

--- TypeScript/Property Rental Manger App/app.js
+++ TypeScript/Property Rental Manger App/app.js
@@ -1,10 +1,16 @@
 function getAllHouses() {
     var houses = [
-        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false },
-        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true },
-        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true },
-        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false }
+        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false, typeOfDwelling: [typeOfDwelling.rambler] },
+        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true, typeOfDwelling: [typeOfDwelling.twostory] },
+        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true, typeOfDwelling: [typeOfDwelling.duplex] },
+        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false, typeOfDwelling: [typeOfDwelling.twostory] }
     ];
+    var typeOfDwelling;
+    (function (typeOfDwelling) {
+        typeOfDwelling[typeOfDwelling["rambler"] = 0] = "rambler";
+        typeOfDwelling[typeOfDwelling["twostory"] = 1] = "twostory";
+        typeOfDwelling[typeOfDwelling["duplex"] = 2] = "duplex";
+    })(typeOfDwelling || (typeOfDwelling = {}));
     return houses;
 }
 function logFirstAvailable(houses) {
@@ -22,3 +28,13 @@ function logFirstAvailable(houses) {
     console.log('First Available house is ' + firstAvailable);
 }
 logFirstAvailable();
+function getHousesByTypeOfDwelling(houses) {
+    if (houses === void 0) { houses = getAllHouses(); }
+    var numberOfHouses = houses.length;
+    var firstFound;
+    for (var _i = 0, houses_2 = houses; _i < houses_2.length; _i++) {
+        var currentHouse = houses_2[_i];
+        if (currentHouse.)
+            ;
+    }
+}
--- TypeScript/Property Rental Manger App/app.ts
+++ TypeScript/Property Rental Manger App/app.ts
@@ -1,11 +1,16 @@
 
 function getAllHouses () {
     let houses = [
-        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false},
-        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true},
-        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true},
-        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false}
+        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false, typeOfDwelling: [typeOfDwelling.rambler]},
+        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true, typeOfDwelling: [typeOfDwelling.twostory]},
+        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true, typeOfDwelling: [typeOfDwelling.duplex]},
+        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false, typeOfDwelling: [typeOfDwelling.twostory]}
     ];
+    enum typeOfDwelling {
+        rambler,
+        twostory,
+        duplex
+    }
     return houses;
 }
 
@@ -23,3 +28,11 @@ function logFirstAvailable(houses = getAllHouses()): void{
 }
 
 logFirstAvailable();
+
+function getHousesByTypeOfDwelling (houses = getAllHouses()): void{
+    let numberOfHouses: number = houses.length;
+    let firstFound: number;
+    for(let currentHouse of houses){
+        if(currentHouse.)
+    }
+}
