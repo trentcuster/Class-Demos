c71ac6ff14211b5347f00c91d6f6ce77e74cf3f9
trentcuster <tbcuster@gmail.com>
2017-05-05 10:27:00 -0600

    enums

--- TypeScript/Property Rental Manger App/app.js
+++ TypeScript/Property Rental Manger App/app.js
@@ -1,18 +1,19 @@
 function getAllHouses() {
     var houses = [
-        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false, typeOfDwelling: [typeOfDwelling.rambler] },
-        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true, typeOfDwelling: [typeOfDwelling.twostory] },
-        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true, typeOfDwelling: [typeOfDwelling.duplex] },
-        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false, typeOfDwelling: [typeOfDwelling.twostory] }
+        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false, typeOfDwelling: typeOfDwelling.rambler },
+        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true, typeOfDwelling: typeOfDwelling.twostory },
+        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true, typeOfDwelling: typeOfDwelling.duplex },
+        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false, typeOfDwelling: typeOfDwelling.twostory }
     ];
-    var typeOfDwelling;
-    (function (typeOfDwelling) {
-        typeOfDwelling[typeOfDwelling["rambler"] = 0] = "rambler";
-        typeOfDwelling[typeOfDwelling["twostory"] = 1] = "twostory";
-        typeOfDwelling[typeOfDwelling["duplex"] = 2] = "duplex";
-    })(typeOfDwelling || (typeOfDwelling = {}));
     return houses;
 }
+//seperate file for enums
+var typeOfDwelling;
+(function (typeOfDwelling) {
+    typeOfDwelling[typeOfDwelling["rambler"] = 0] = "rambler";
+    typeOfDwelling[typeOfDwelling["twostory"] = 1] = "twostory";
+    typeOfDwelling[typeOfDwelling["duplex"] = 2] = "duplex";
+})(typeOfDwelling || (typeOfDwelling = {}));
 function logFirstAvailable(houses) {
     if (houses === void 0) { houses = getAllHouses(); }
     var numberOfHouses = houses.length;
@@ -34,7 +35,7 @@ function getHousesByTypeOfDwelling(houses) {
     var firstFound;
     for (var _i = 0, houses_2 = houses; _i < houses_2.length; _i++) {
         var currentHouse = houses_2[_i];
-        if (currentHouse.)
-            ;
+        if (currentHouse.typeOfDwelling === typeOfDwelling.twostory) {
+        }
     }
 }
--- TypeScript/Property Rental Manger App/app.ts
+++ TypeScript/Property Rental Manger App/app.ts
@@ -1,17 +1,19 @@
 
 function getAllHouses () {
     let houses = [
-        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false, typeOfDwelling: [typeOfDwelling.rambler]},
-        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true, typeOfDwelling: [typeOfDwelling.twostory]},
-        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true, typeOfDwelling: [typeOfDwelling.duplex]},
-        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false, typeOfDwelling: [typeOfDwelling.twostory]}
+        { id: 1, address: 111, numRooms: 1, numBaths: 1, squareFeet: 1111, available: false, typeOfDwelling: typeOfDwelling.rambler},
+        { id: 2, address: 222, numRooms: 2, numBaths: 2, squareFeet: 2222, available: true, typeOfDwelling: typeOfDwelling.twostory},
+        { id: 3, address: 333, numRooms: 3, numBaths: 3, squareFeet: 3333, available: true, typeOfDwelling: typeOfDwelling.duplex},
+        { id: 4, address: 444, numRooms: 4, numBaths: 4, squareFeet: 4444, available: false, typeOfDwelling: typeOfDwelling.twostory}
     ];
-    enum typeOfDwelling {
-        rambler,
-        twostory,
-        duplex
-    }
+
     return houses;
+}
+//seperate file for enums
+enum typeOfDwelling {
+    rambler,
+    twostory,
+    duplex
 }
 
 function logFirstAvailable(houses = getAllHouses()): void{
@@ -33,6 +35,9 @@ function getHousesByTypeOfDwelling (houses = getAllHouses()): void{
     let numberOfHouses: number = houses.length;
     let firstFound: number;
     for(let currentHouse of houses){
-        if(currentHouse.)
+        if(currentHouse.typeOfDwelling === typeOfDwelling.twostory){
+
+        }
     }
 }
+
